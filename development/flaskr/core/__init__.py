0bb1e184172a4701ac1e22e5a18de368846f80a0
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-02-24 18:23:04 +0700

    🌾 Added core utilities

---
+++ development/flaskr/core/__init__.py
@@ -0,0 +1,4 @@
+from .config import dev_settings
+from .config import prod_settings
+from .config import test_settings
+from .logs import logger
---
+++ development/flaskr/core/config.py
@@ -0,0 +1,73 @@
+from datetime import timedelta
+
+from secrets import token_urlsafe
+
+from typing import Any
+from typing import List
+from typing import Optional
+
+from decouple import config
+
+from pydantic import field_validator
+from pydantic import ValidationInfo
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=True, env_file_encoding="utf-8", env_file=".env"
+    )
+
+    # Flask Configuration
+    FLASK_APP: str
+    SECRET_KEY: str = token_urlsafe(16)
+    API_V1_STR: str = "/api/v1"
+    DEBUG: bool = True
+    TESTING: bool = False
+
+    # SQLAlchemy Configuration
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str = config("POSTGRES_HOST", default="localhost")
+    SQLALCHEMY_DATABASE_URI: Optional[str] = None
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
+
+    @field_validator("SQLALCHEMY_DATABASE_URI")
+    def assemble_db_connection(cls, v: Optional[str], info: ValidationInfo) -> Any:
+        if isinstance(v, str):
+            return v
+
+        return f"postgresql://{info.data.get('POSTGRES_USER')}:{info.data.get('POSTGRES_PASSWORD')}@{info.data.get('POSTGRES_HOST')}:5432/{info.data.get('POSTGRES_DB')}?sslmode=disable"
+
+    # JWT Configuration
+    JWT_COOKIE_SECURE: bool = config("JWT_COOKIE_SECURE", default=False)
+    JWT_TOKEN_LOCATION: List[str]
+    JWT_SECRET_KEY: str = token_urlsafe(16)
+    JWT_ACCESS_TOKEN_EXPIRES: timedelta = timedelta(days=2)
+
+    # Bcrypt Configuration
+    BCRYPT_LOG_ROUNDS: int = 13
+
+    # WTF Configuration
+    WTF_CSRF_ENABLED: bool = True
+
+
+class DevelopmentSettings(Settings):
+    DEVELOPMENT: bool = True
+    DEBUG: bool = True
+    WTF_CSRF_ENABLED: bool = False
+
+
+class TestingSettings(Settings):
+    TESTING: bool = True
+    DEBUG: bool = True
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:///testdb.sqlite"
+    BCRYPT_LOG_ROUNDS: int = 5
+    WTF_CSRF_ENABLED: bool = False
+
+
+prod_settings = Settings()
+dev_settings = DevelopmentSettings()
+test_settings = TestingSettings()
